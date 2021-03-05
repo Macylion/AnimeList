5483fff84d9137add6eaa6b422fab51caa49e6e0
Sorshi <you@example.com>
2021-03-06 00:34:42 +0100

    karuzela myk myk

--- src/components/TilesCarousel.vue
+++ src/components/TilesCarousel.vue
@@ -1,11 +1,20 @@
 <template>
 	<div id="tiles-carousel-container">
 		<flickity ref="flickity" :options="flickityOptions">
-			<div class="carousel-cell">1</div>
-			<div class="carousel-cell">2</div>
-			<div class="carousel-cell">3</div>
-			<div class="carousel-cell">4</div>
-			<div class="carousel-cell">5</div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
 		</flickity>
 	</div>
 </template>
@@ -20,10 +29,12 @@ export default {
   data() {
     return {
       flickityOptions: {
-        initialIndex: 3,
-        prevNextButtons: true,
-        pageDots: true,
-        wrapAround: true
+		initialIndex: 1, //od którego się zaczyna
+		autoPlay: 5000, //auto play w milisekundach
+		prevNextButtons: false, //przyciski
+		pageDots: false, //kropki
+		wrapAround: true, // nieskończona karuzela
+		groupCells: 4 //grupowanie po 4 kafelki
         
         // any options from Flickity can be used
       }
@@ -44,11 +55,22 @@ export default {
 
 <style lang="scss">
 	#tiles-carousel-container{
-		margin-top: 38px;
+		margin-top: 53px;
+		margin-left: 198px;
+		width: 1514px; 
+		//margin-right: 208px;
 		.carousel-cell{
 			width: 362px;
 			height: 238px;
 			margin-right: 22px;
+			background-image: url("https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png");
+			border-radius: 2px;
+			background-position: center;
+			.tiles-carousel-shadow{
+				width: 362px;
+				height: 238px;
+				background: rgba(38, 38, 38, 0.4);
+			}
 		}
 	}
 
