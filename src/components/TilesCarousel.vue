93610aea560ad729d6a7b901cf03093c6c7bc376
Sorshi <you@example.com>
2021-03-06 01:16:37 +0100

    karuzela myk 2

--- src/components/TilesCarousel.vue
+++ src/components/TilesCarousel.vue
@@ -1,20 +1,114 @@
 <template>
 	<div id="tiles-carousel-container">
 		<flickity ref="flickity" :options="flickityOptions">
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
-			<div class="carousel-cell"><div class="tiles-carousel-shadow"></div></div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
+			<div class="carousel-cell">
+				<div class="tiles-carousel-shadow"></div>
+				<div class="tiles-carousel-shadow2"></div>
+				<div class="tiles-carousel-info">
+				<span class="tiles-carousel-titles">Darling in the FranXX</span><br/><span class="tiles-carousel-titles-gray">Anime 2017</span>
+				</div>
+				<img class="tiles-carousel-info-img" src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png">
+				<div class="tiles-carousel-ep-info">Next ep 2d 5h 40min</div>
+			</div>
 		</flickity>
 	</div>
 </template>
@@ -66,10 +160,60 @@ export default {
 			background-image: url("https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx99423-8MBxtwCeHf8B.png");
 			border-radius: 2px;
 			background-position: center;
+			overflow: hidden;
+			.tiles-carousel-info-img{
+				position: absolute;
+				width: 59px;
+				bottom: 16px;
+				right: 16px;
+				border-radius: 2px;
+			}
+			.tiles-carousel-ep-info{
+				border-radius: 2px;
+				position: absolute;
+				width: 154px;
+				height: 20px;
+				padding-top: 3px;
+				left: 122px;
+				top: 200px;	
+				font-family: var(--rubik);
+				font-weight: var(--fontBold);
+				text-align: center;
+				font-size: 12px;
+				color: var(--light);
+				background-color: var(--blue);
+				letter-spacing: 0.5px;
+			}
 			.tiles-carousel-shadow{
+				position: absolute;
 				width: 362px;
 				height: 238px;
 				background: rgba(38, 38, 38, 0.4);
+				border-radius: 2px;
+			}
+			.tiles-carousel-shadow2{
+				position: absolute;
+				width: 362px;
+				height: 238px;
+				background: linear-gradient(359.63deg, rgba(0, 0, 0, 0.8) 0.2%, rgba(15, 15, 15, 0.495833) 70.59%, rgba(38, 38, 38, 0) 99.57%);
+				border-radius: 2px;
+			}
+			.tiles-carousel-info{
+				position: absolute;
+				width: 126px;
+				height: 40px;
+				left: 149px;
+				top: 136px;
+				text-align: right;
+				.tiles-carousel-titles{
+					color: var(--light);
+				}
+				.tiles-carousel-titles-gray{
+					font-family: var(--roboto);
+					font-weight: var(--fontLight);	
+					color: var(--light);
+					opacity: 0.5;
+				}
 			}
 		}
 	}
